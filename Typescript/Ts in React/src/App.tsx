ba02ad9f67c5128e3ec3e072b9baa3f15b9a4004
Sahil Shaikh <154968980+Sahil-tried-coding@users.noreply.github.com>
2025-03-24 11:23:31 -0500

    Update App.tsx
    h

--- Typescript/Ts in React/src/App.tsx
+++ Typescript/Ts in React/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       {/* hello world this is typescript in react
       <Props name='sahil' age={21}/>
       <h2>hi thid id djd f</h2> */}
-      <UseState/>
+{/*       <UseState/> */}
       <ThemeWala/>
       <Counter/>
       {/* <Reducers/> */}
